55eb94d394e0560f3ac133261b49fc930080b68f
Momen <100791622+momenatallah@users.noreply.github.com>
2022-07-17 16:26:35 +0200

    Update Debugging-challenge.py

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Debugging-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Debugging-challenge.py
@@ -1,1 +1,14 @@
-
+# define function for plant if care low for aloe, if care medium for pothos, if care  high for orchid.
+def plantRecommendation(care):
+# I find syntax error '='
+    if care == 'low':
+        print('aloe')
+    elif care == 'medium':
+        print('pothos')      
+# I find logical error 'medium'
+    elif care == 'high':
+        print('orchid')       
+# I find runtime error 'plant_rec'
+plantRecommendation('low')
+plantRecommendation('medium')
+plantRecommendation('high')

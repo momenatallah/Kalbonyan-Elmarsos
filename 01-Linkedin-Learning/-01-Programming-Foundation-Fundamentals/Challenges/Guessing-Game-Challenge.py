6012f552abd399b2bd44f430af469019b3d8575a
Momen <100791622+momenatallah@users.noreply.github.com>
2022-07-17 16:20:15 +0200

    Update Guessing-Game-Challenge.py

--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Guessing-Game-Challenge.py
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Guessing-Game-Challenge.py
@@ -1,1 +1,10 @@
 
+favFood = input("What's my favorite food?")
+print(favFood)
+
+if (favFood == "Chicken" or favFood =="chicken") :
+    print("Yes! So Tasty!")
+else :
+    print("Yucky!")
+
+print("Thanks for playing!")

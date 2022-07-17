056c759381ca24ad0b632613bf4f6935b4d446d7
Momen <100791622+momenatallah@users.noreply.github.com>
2022-07-17 16:27:15 +0200

    Update For-loop-challenge.py

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/For-loop-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/For-loop-challenge.py
@@ -1,1 +1,17 @@
 
+# Create list variable fruits and set all fruits
+fruits = [
+    'apples',
+    'mangos',
+    'dragon fruit',
+    'bananas',
+    'pears',
+    'nectarines'
+]
+
+# Print "Our fruit selection:"
+print('Our fruit selection:')
+
+
+for fruit in fruits:
+    print(fruit)

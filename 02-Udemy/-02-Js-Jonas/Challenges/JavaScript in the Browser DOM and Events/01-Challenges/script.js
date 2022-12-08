d750f48dffb4d00dbd5c76a9a85fa5dc1063443c
Momen <100791622+momenatallah@users.noreply.github.com>
2022-12-08 18:37:10 +0100

    challenge #1 - dom

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript in the Browser DOM and Events/01-Challenges/script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let random = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess || guess < 1 || guess > 20) {
+    document.querySelector('.message').textContent =
+      'Please enter a number between 1 and 20!';
+  } else if (guess === random) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = random;
+    if (score > Number(document.querySelector('.highscore').textContent)) {
+      document.querySelector('.highscore').textContent = score;
+    }
+  } else if (guess > random) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < random) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  random = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
